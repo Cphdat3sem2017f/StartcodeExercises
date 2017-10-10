7a14e98e0896c9979e21ca0d40a1f57dc98d8967
Lars-m <lam@cphbusiness.dk>
2017-10-10 14:19:16 +0200

    Create Calculator.js

---
+++ React/Calculator.js
@@ -0,0 +1,120 @@
+import React from "react";
+import {StyleSheet} from "react"
+
+/* ############ Utility Functions / Objects  ############## */
+/* These are pure JavaScript functions / objects (no React) */
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+const styles =  {
+  borderRadius: 4,
+  borderWidth: 1,
+  borderColor: 'steelBlue',
+  borderStyle: "solid",
+  backgroundColor: "red",
+  margin: 40,
+  paddingLeft: 5,
+  paddingRight: 5,
+  paddingBottom: 5,
+  width: 300,
+}
+
+/* --------------- End of Utility Functions -------------------- */
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+/* ####### React Components ######### */
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {temperature: '', scale: 'c'};
+  }
+
+  handleCelsiusChange(temperature) {
+    //Observe the shourcut way of setting a property in an object with the value frp, a variable with the 
+    //same name as the property.Below, is the same as: this.setState({scale: 'c', temperature: temperature});
+    this.setState({scale: 'c', temperature});
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <div style={styles}>
+      <h3>Temperature Converter</h3>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange} />
+        <br/>
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange} />
+
+        <BoilingVerdict
+          celsius={parseFloat(celsius)} />
+
+      </div>
+    );
+  }
+}
